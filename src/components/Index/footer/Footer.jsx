5aff1c0edc5178e09cde84c0b47d7af535074876
Syed Muhammed Bilal <intergo878@gmail.com>
2021-03-21 12:18:39 +0500

    new ui

--- src/components/Index/footer/Footer.jsx
+++ src/components/Index/footer/Footer.jsx
@@ -8,6 +8,7 @@ import SkipNextIcon from "@material-ui/icons/SkipNext";
 import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 // import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import { Grid, Slider } from "@material-ui/core";
 import './Footer.css'
 
@@ -15,17 +16,32 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer__left">
-
+                <img src="https://pyxis.nymag.com/v1/imgs/ff4/bf1/0676dc29d8cbfb939ea079638371ffc44d-justice.rsquare.w1200.jpg" className="footer__albumLogo" alt="cover" />
+                <div className="footer__songInfo">
+                    <h4>Hold On</h4>
+                    <p>Justin Bieber</p>
+                </div>
+                <FavoriteIcon className="footer_likeButton" style={{fontSize: '18px'}} />
             </div>
             <div className="footer__center">
-                <ShuffleIcon className="footer__green" />
-                <SkipPreviousIcon className="footer__icon" />
-                <PlayCircleFilledWhiteIcon fontSize="large" className="footer__icon" />
-                <SkipNextIcon className="footer__icon" />
-                <RepeatIcon className="footer__green" />
+                <ShuffleIcon className="footer__green footer__sideIcon" />
+                <SkipPreviousIcon className="footer__icon footer__sideIcon" />
+                <PlayCircleFilledWhiteIcon fontSize="large" style={{color: '#fff'}} className="footer__icon footer__playButton" />
+                <SkipNextIcon className="footer__icon footer__sideIcon" />
+                <RepeatIcon className="footer__green footer__sideIcon" />
             </div>
             <div className="footer__right">
-
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider style={{color: 'rgb(160, 159, 159)'}} />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )
